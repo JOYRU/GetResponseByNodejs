5e0a7acca533dc15574c2a63e4524cb264a5c347
JOY_RU <joycseru@gmail.com>
2018-04-03 16:57:34 +0600

    build a restful app which return response from a url

---
+++ server.js
@@ -0,0 +1,28 @@
+var https = require("https");
+
+var url = "https://www.google.com/pretend/this/exists.xml";
+
+var opt = {
+    host: url.split(".com/")[0] + ".com",
+    path: "/" + url.split(".com/")[1]
+};
+
+function callback(response) {
+    var str = "hello";
+
+    response.on("data", function (chunk) {
+        str += chunk;
+    });
+
+    response.on("end", function () {
+        console.log(str);
+    });
+}
+
+var request = https.request(opt, callback);
+
+request.on("error", function (error) {
+    console.error(error);
+});
+
+request.end();
